1dab450cd47d73c35131a1840e7eaafe3abaa083
taranbeer singh <62498540+staranbeer@users.noreply.github.com>
2020-11-03 11:09:35 +0530

    modified styles and added stats

--- script.js
+++ script.js
@@ -1,19 +1,28 @@
-const alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "Backspace", " "]
-const textBox = "this is a sample paragraph that we are going to use to check if our algorthm works or not even though i know it could take a long time to check if it works or not however i cannot rest easy until i do so as it is one of my projects that i can use on my portfolio"
-const text = textBox.split('')
+const alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", " "]
+
+
+//displaying the text we are going to use into the text area
+const textBox = "lorem ipsum dolor sit amet consectetur adipisicing elit Recusandae illo rerum ex praesentium at exercitationem placeat Ipsa sapiente dolor quas repellendus ab officia ut tempora alias molestias laborum similique eveniet"
+const text = textBox.trim().toLowerCase().split('')
 const textArea = document.getElementById('text-box')
-let current = 0
 
 text.forEach(i=>{
-   const char = document.createElement('span')
-   char.innerText = i
+    const char = document.createElement('span')
+    char.innerText = i
     char.className = "char"
     textArea.appendChild(char)
 })
 
 const chars = document.getElementsByClassName('char')
-console.log(chars[0])
-console.log(text)
+let current = 0
+let wrongAnswers = 0;
+let rightAnswers = 0;
+let numberOfChracters = text.length;
+
+const right = document.getElementById('right')
+const wrong = document.getElementById('wrong')
+const total = document.getElementById('total')
+//changing the style of the keys based on key up and key down events
 
 function keydownStyle(e){
         if(e.key == " "){
@@ -43,6 +52,7 @@ function keyUpStyle(e){
         character.style.color = "#ffffff"
 }
 
+//a helper function to check if the key is a valid character
 
 function findKey(key){
     for(let i = 0; i< alphabet.length; i++){
@@ -53,32 +63,39 @@ function findKey(key){
     return false
 }
 
-
-
-
+// key up and key down handlers
 
 function keyDownHandler(e){
+    //if the key pressed is valid change the background color
     if(findKey(e.key)){
         keydownStyle(e);
     }
 
+    //if the key key pressed is the current key they change it to green
+    //else change it to red
     if(chars[current].innerText == e.key.toString()){
-        chars[current].style.color = "#00FF00"
+        chars[current].style.color = "rgb(147, 255, 86)"
+        rightAnswers++
+        right.innerText = rightAnswers
+
     }else{
-        chars[current].style.color = "#fe0000"
+        chars[current].style.color = "rgb(255, 95, 83)"
+        wrongAnswers++;
+        wrong.innerText = wrongAnswers
     }
     current++
+    console.log(`right: ${rightAnswers}, wrong: ${wrongAnswers}`)
+
 }
 
-
 function keyUpHandler(e){
-
+    //if the key pressed is valid change the background color
     if(findKey(e.key)){
         keyUpStyle(e) 
     } 
     
 }
 
-
 document.addEventListener('keydown', keyDownHandler)
 document.addEventListener('keyup', keyUpHandler)
+
